59dd1b7351cc46dd2b88868125b1279e9fb60070
Yijun Tang <yijuntang94@gmail.com>
2024-04-17 15:35:43 +0800

    replace RefCell with RwLock

--- src/adlist.rs
+++ src/adlist.rs
@@ -1,16 +1,16 @@
 //! A generic doubly linked list implementation.
 
-use std::{cell::RefCell, ops::Deref, sync::{Arc, Weak}};
+use std::{ops::Deref, sync::{Arc, RwLock, Weak}};
 
 #[derive(Debug)]
 pub struct ListNode<T> {
-    prev: Option<Weak<RefCell<ListNode<T>>>>,
-    next: Option<Arc<RefCell<ListNode<T>>>>,
+    prev: Option<Weak<RwLock<ListNode<T>>>>,
+    next: Option<Arc<RwLock<ListNode<T>>>>,
     value: Option<Arc<T>>,
 }
 
 impl<T> ListNode<T> {
-    pub fn prev(&self) -> Option<Arc<RefCell<ListNode<T>>>> {
+    pub fn prev(&self) -> Option<Arc<RwLock<ListNode<T>>>> {
         if let Some(p) = self.prev.clone() {
             if let Some(p) = p.upgrade() {
                 return Some(p);
@@ -19,7 +19,7 @@ impl<T> ListNode<T> {
         None
     }
 
-    pub fn next(&self) -> Option<Arc<RefCell<ListNode<T>>>> {
+    pub fn next(&self) -> Option<Arc<RwLock<ListNode<T>>>> {
         self.next.clone()
     }
 
@@ -30,8 +30,8 @@ impl<T> ListNode<T> {
 
 #[derive(Debug)]
 pub struct List<T> {
-    head: Option<Arc<RefCell<ListNode<T>>>>,
-    tail: Option<Arc<RefCell<ListNode<T>>>>,
+    head: Option<Arc<RwLock<ListNode<T>>>>,
+    tail: Option<Arc<RwLock<ListNode<T>>>>,
     len: usize,
 }
 
@@ -44,7 +44,7 @@ impl<T: PartialEq> List<T> {
     /// Add a new node to the list, to head, contaning the specified 'value' 
     /// pointer as value.
     pub fn add_node_head(&mut self, value: Option<Arc<T>>) -> &mut Self {
-        let node = Arc::new(RefCell::new(
+        let node = Arc::new(RwLock::new(
             ListNode { prev: None, next: self.head.take(), value }
         ));
         if self.len == 0 {
@@ -52,8 +52,8 @@ impl<T: PartialEq> List<T> {
             self.tail = Some(node.clone());
         } else {
             self.head = Some(node.clone());
-            if let Some(p) = node.deref().borrow().next.clone() {
-                p.deref().borrow_mut().prev = Some(Arc::downgrade(&node));
+            if let Some(p) = node.read().unwrap().next.clone() {
+                p.write().unwrap().prev = Some(Arc::downgrade(&node));
             }
         }
         self.len += 1;
@@ -63,7 +63,7 @@ impl<T: PartialEq> List<T> {
     /// Add a new node to the list, to tail, contaning the specified 'value' 
     /// pointer as value.
     pub fn add_node_tail(&mut self, value: Option<Arc<T>>) -> &mut Self {
-        let node = Arc::new(RefCell::new(
+        let node = Arc::new(RwLock::new(
             ListNode { prev: None, next: None, value }
         ));
         if self.len == 0 {
@@ -71,8 +71,8 @@ impl<T: PartialEq> List<T> {
             self.tail = Some(node.clone());
         } else {
             if let Some(t) = self.tail.clone() {
-                t.deref().borrow_mut().next = Some(node.clone());
-                node.deref().borrow_mut().prev = Some(Arc::downgrade(&t));
+                t.write().unwrap().next = Some(node.clone());
+                node.write().unwrap().prev = Some(Arc::downgrade(&t));
                 self.tail = Some(node.clone());
             }
         }
@@ -84,23 +84,23 @@ impl<T: PartialEq> List<T> {
     /// The caller shouldn't hold the reference to the node. 
     /// 
     /// Assumption: the node belongs to the list.
-    pub fn del_node(&mut self, node: Option<Arc<RefCell<ListNode<T>>>>) {
+    pub fn del_node(&mut self, node: Option<Arc<RwLock<ListNode<T>>>>) {
         if let Some(node) = node {
-            if node.deref().borrow().prev.is_some() {
-                if let Some(p) = node.deref().borrow().prev.clone() {
+            if node.read().unwrap().prev.is_some() {
+                if let Some(p) = node.read().unwrap().prev.clone() {
                     if let Some(p) = p.upgrade() {
-                        p.deref().borrow_mut().next = node.deref().borrow().next.clone();
+                        p.write().unwrap().next = node.read().unwrap().next.clone();
                     }
                 }
             } else {
-                self.head = node.deref().borrow().next.clone();
+                self.head = node.read().unwrap().next.clone();
             }
-            if node.deref().borrow().next.is_some() {
-                if let Some(n) = node.deref().borrow().next.clone() {
-                    n.deref().borrow_mut().prev = node.deref().borrow().prev.clone();
+            if node.read().unwrap().next.is_some() {
+                if let Some(n) = node.read().unwrap().next.clone() {
+                    n.write().unwrap().prev = node.read().unwrap().prev.clone();
                 }
             } else {
-                if let Some(p) = node.deref().borrow().prev.clone() {
+                if let Some(p) = node.read().unwrap().prev.clone() {
                     self.tail = p.upgrade();
                 }
             }
@@ -137,12 +137,12 @@ impl<T: PartialEq> List<T> {
     }
 
     /// Search the list for a node matching a given key.
-    pub fn search_key(&self, key: Option<Arc<T>>) -> Option<Arc<RefCell<ListNode<T>>>> {
+    pub fn search_key(&self, key: Option<Arc<T>>) -> Option<Arc<RwLock<ListNode<T>>>> {
         if let Some(key) = key {
             let mut iter = self.iter(IterDir::AlStartHead);
         
             while let Some(node) = iter.next() {
-                if let Some(n) = node.deref().borrow().value() {
+                if let Some(n) = node.read().unwrap().value() {
                     if n == key {
                         return Some(node.clone());
                     }
@@ -157,14 +157,14 @@ impl<T: PartialEq> List<T> {
     /// and so on. Negative integers are used in order to count
     /// from the tail, -1 is the last element, -2 the penultimante
     /// and so on. If the index is out of range None is returned.
-    pub fn index(&self, mut index: isize) -> Option<Arc<RefCell<ListNode<T>>>> {
-        let mut _n: Option<Arc<RefCell<ListNode<T>>>> = None;
+    pub fn index(&self, mut index: isize) -> Option<Arc<RwLock<ListNode<T>>>> {
+        let mut _n: Option<Arc<RwLock<ListNode<T>>>> = None;
 
         if index < 0 {
             index = (-index) - 1;
             _n = self.tail.clone();
             while index > 0 && _n.is_some() {
-                if let Some(p) = _n.unwrap().deref().borrow().prev.clone() {
+                if let Some(p) = _n.unwrap().read().unwrap().prev.clone() {
                     if let Some(p) = p.upgrade() {
                         _n = Some(p);
                     } else {
@@ -178,7 +178,7 @@ impl<T: PartialEq> List<T> {
         } else {
             _n = self.head.clone();
             while index > 0 && _n.is_some() {
-                _n = _n.unwrap().deref().borrow().next.clone();
+                _n = _n.unwrap().read().unwrap().next.clone();
                 index -= 1;
             }
         }
@@ -191,11 +191,11 @@ impl<T: PartialEq> List<T> {
         self.len
     }
 
-    pub fn first(&self) -> Option<Arc<RefCell<ListNode<T>>>> {
+    pub fn first(&self) -> Option<Arc<RwLock<ListNode<T>>>> {
         self.head.clone()
     }
 
-    pub fn last(&self) -> Option<Arc<RefCell<ListNode<T>>>> {
+    pub fn last(&self) -> Option<Arc<RwLock<ListNode<T>>>> {
         self.tail.clone()
     }
 }
@@ -207,7 +207,7 @@ impl<T: PartialEq + Clone> Clone for List<T> {
 
         let mut iter = self.iter(IterDir::AlStartHead);
         while let Some(node) = iter.next() {
-            if let Some(n) = node.deref().borrow().value() {
+            if let Some(n) = node.read().unwrap().value() {
                 clone.add_node_tail(Some(Arc::new(n.deref().clone())));
             }
         }
@@ -216,7 +216,7 @@ impl<T: PartialEq + Clone> Clone for List<T> {
 }
 
 pub struct ListIter<T> {
-    next: Option<Arc<RefCell<ListNode<T>>>>,
+    next: Option<Arc<RwLock<ListNode<T>>>>,
     direction: IterDir,
 }
 
@@ -224,15 +224,15 @@ impl<T> ListIter<T> {
     /// Return the next element of an iterator.
     /// It's valid to remove the currently returned element using 
     /// `del_node()`, but not to remove other elements.
-    pub fn next(&mut self) -> Option<Arc<RefCell<ListNode<T>>>> {
+    pub fn next(&mut self) -> Option<Arc<RwLock<ListNode<T>>>> {
         if let Some(c) = self.next.clone() {
             match self.direction {
                 IterDir::AlStartHead => {
-                    self.next = c.deref().borrow().next.clone();
+                    self.next = c.read().unwrap().next.clone();
                     Some(c.clone())
                 },
                 IterDir::AlStartTail => {
-                    if let Some(p) = c.deref().borrow().prev.clone() {
+                    if let Some(p) = c.read().unwrap().prev.clone() {
                         if let Some(p) = p.upgrade() {
                             self.next = Some(p);
                             return Some(c.clone());
@@ -264,35 +264,35 @@ mod tests {
             .add_node_tail(Some(Arc::new(2)))
             .add_node_tail(Some(Arc::new(3)))
             .add_node_tail(Some(Arc::new(4)));
-        assert_eq!(format!("{:?}", list), "List { head: Some(RefCell { value: ListNode { prev: None, next: Some(RefCell { value: ListNode { prev: Some((Weak)), next: Some(RefCell { value: ListNode { prev: Some((Weak)), next: Some(RefCell { value: ListNode { prev: Some((Weak)), next: None, value: Some(4) } }), value: Some(3) } }), value: Some(2) } }), value: Some(1) } }), tail: Some(RefCell { value: ListNode { prev: Some((Weak)), next: None, value: Some(4) } }), len: 4 }");
+        assert_eq!(format!("{:?}", list), "List { head: Some(RwLock { data: ListNode { prev: None, next: Some(RwLock { data: ListNode { prev: Some((Weak)), next: Some(RwLock { data: ListNode { prev: Some((Weak)), next: Some(RwLock { data: ListNode { prev: Some((Weak)), next: None, value: Some(4) }, poisoned: false, .. }), value: Some(3) }, poisoned: false, .. }), value: Some(2) }, poisoned: false, .. }), value: Some(1) }, poisoned: false, .. }), tail: Some(RwLock { data: ListNode { prev: Some((Weak)), next: None, value: Some(4) }, poisoned: false, .. }), len: 4 }");
 
         let mut iter = list.iter(IterDir::AlStartTail);
         if let Some(node) = iter.next() {
-            assert_eq!(format!("{:?}", node), "RefCell { value: ListNode { prev: Some((Weak)), next: None, value: Some(4) } }");
+            assert_eq!(format!("{:?}", node), "RwLock { data: ListNode { prev: Some((Weak)), next: None, value: Some(4) }, poisoned: false, .. }");
         }
         if let Some(node) = iter.next() {
-            assert_eq!(format!("{:?}", node), "RefCell { value: ListNode { prev: Some((Weak)), next: Some(RefCell { value: ListNode { prev: Some((Weak)), next: None, value: Some(4) } }), value: Some(3) } }");
+            assert_eq!(format!("{:?}", node), "RwLock { data: ListNode { prev: Some((Weak)), next: Some(RwLock { data: ListNode { prev: Some((Weak)), next: None, value: Some(4) }, poisoned: false, .. }), value: Some(3) }, poisoned: false, .. }");
         }
         if let Some(node) = iter.next() {
-            assert_eq!(format!("{:?}", node), "RefCell { value: ListNode { prev: Some((Weak)), next: Some(RefCell { value: ListNode { prev: Some((Weak)), next: Some(RefCell { value: ListNode { prev: Some((Weak)), next: None, value: Some(4) } }), value: Some(3) } }), value: Some(2) } }");
+            assert_eq!(format!("{:?}", node), "RwLock { data: ListNode { prev: Some((Weak)), next: Some(RwLock { data: ListNode { prev: Some((Weak)), next: Some(RwLock { data: ListNode { prev: Some((Weak)), next: None, value: Some(4) }, poisoned: false, .. }), value: Some(3) }, poisoned: false, .. }), value: Some(2) }, poisoned: false, .. }");
         }
         if let Some(node) = iter.next() {
-            assert_eq!(format!("{:?}", node), "RefCell { value: ListNode { prev: None, next: Some(RefCell { value: ListNode { prev: Some((Weak)), next: Some(RefCell { value: ListNode { prev: Some((Weak)), next: Some(RefCell { value: ListNode { prev: Some((Weak)), next: None, value: Some(4) } }), value: Some(3) } }), value: Some(2) } }), value: Some(1) } }");
+            assert_eq!(format!("{:?}", node), "RwLock { data: ListNode { prev: None, next: Some(RwLock { data: ListNode { prev: Some((Weak)), next: Some(RwLock { data: ListNode { prev: Some((Weak)), next: Some(RwLock { data: ListNode { prev: Some((Weak)), next: None, value: Some(4) }, poisoned: false, .. }), value: Some(3) }, poisoned: false, .. }), value: Some(2) }, poisoned: false, .. }), value: Some(1) }, poisoned: false, .. }");
         }
         assert!(iter.next().is_none());
 
         let mut iter = list.iter(IterDir::AlStartHead);
         if let Some(node) = iter.next() {
-            assert_eq!(format!("{:?}", node), "RefCell { value: ListNode { prev: None, next: Some(RefCell { value: ListNode { prev: Some((Weak)), next: Some(RefCell { value: ListNode { prev: Some((Weak)), next: Some(RefCell { value: ListNode { prev: Some((Weak)), next: None, value: Some(4) } }), value: Some(3) } }), value: Some(2) } }), value: Some(1) } }");
+            assert_eq!(format!("{:?}", node), "RwLock { data: ListNode { prev: None, next: Some(RwLock { data: ListNode { prev: Some((Weak)), next: Some(RwLock { data: ListNode { prev: Some((Weak)), next: Some(RwLock { data: ListNode { prev: Some((Weak)), next: None, value: Some(4) }, poisoned: false, .. }), value: Some(3) }, poisoned: false, .. }), value: Some(2) }, poisoned: false, .. }), value: Some(1) }, poisoned: false, .. }");
         }
         if let Some(node) = iter.next() {
-            assert_eq!(format!("{:?}", node), "RefCell { value: ListNode { prev: Some((Weak)), next: Some(RefCell { value: ListNode { prev: Some((Weak)), next: Some(RefCell { value: ListNode { prev: Some((Weak)), next: None, value: Some(4) } }), value: Some(3) } }), value: Some(2) } }");
+            assert_eq!(format!("{:?}", node), "RwLock { data: ListNode { prev: Some((Weak)), next: Some(RwLock { data: ListNode { prev: Some((Weak)), next: Some(RwLock { data: ListNode { prev: Some((Weak)), next: None, value: Some(4) }, poisoned: false, .. }), value: Some(3) }, poisoned: false, .. }), value: Some(2) }, poisoned: false, .. }");
         }
         if let Some(node) = iter.next() {
-            assert_eq!(format!("{:?}", node), "RefCell { value: ListNode { prev: Some((Weak)), next: Some(RefCell { value: ListNode { prev: Some((Weak)), next: None, value: Some(4) } }), value: Some(3) } }");
+            assert_eq!(format!("{:?}", node), "RwLock { data: ListNode { prev: Some((Weak)), next: Some(RwLock { data: ListNode { prev: Some((Weak)), next: None, value: Some(4) }, poisoned: false, .. }), value: Some(3) }, poisoned: false, .. }");
         }
         if let Some(node) = iter.next() {
-            assert_eq!(format!("{:?}", node), "RefCell { value: ListNode { prev: Some((Weak)), next: None, value: Some(4) } }");
+            assert_eq!(format!("{:?}", node), "RwLock { data: ListNode { prev: Some((Weak)), next: None, value: Some(4) }, poisoned: false, .. }");
         }
         assert!(iter.next().is_none());
 
